d3287defb0093aec4d08b61ceddf9c7c36606239
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-10-17 21:11:11 +0300

    123

--- AuthoriseXML/Program.cs
+++ AuthoriseXML/Program.cs
@@ -17,6 +17,21 @@ namespace AuthoriseXML
                     if (AuthoriseFailed) ConsoleCommand.FailedAuth();
                 }
                 if (!AuthoriseFailed) ConsoleCommand.SucessAuth();
+                while (true)
+                {
+                    ConsoleCommand.InformationAfterAuth();
+                    string[] command = Console.ReadLine().Split(" ");
+                    switch (command[0])
+                    {
+                        case "quizlist":; break;
+                        case "startquiz":; break;
+                        case "quizresults":; break;
+                        case "topquiz":; break;
+                        case "changesettings":; break;
+                        case "exit":; break;
+                        default:; break;
+                    }
+                }
             }
             else
             {
@@ -32,157 +47,11 @@ namespace AuthoriseXML
                 Console.WriteLine("Введите дату рождения в формате 2000.12.28");
                 string[] date = Console.ReadLine().Split('.');
                 DateTime dateOfBirth = new DateTime(Int32.Parse(date[0]), Int32.Parse(date[1]), Int32.Parse(date[2]));
-                Authorize.SaveUserData(login,password,dateOfBirth);
+                Authorize.SaveUserData(login, password, dateOfBirth);
             }
-        }
-    }
 
-    class User 
-    {
-        string _name;
-        string _password;
-        DateTime _birthDate;
-        public string Login { get => _name; set  =>  _name = value; }
-        public string Password { get => _password; set => _password = value; }
-        public DateTime DateOfBirth { get => _birthDate; set => _birthDate = value; }
-        public User(string name, string password, DateTime dateOfBirth) 
-        {
-            Login = name;
-            Password = password;
-            DateOfBirth = dateOfBirth;
-        }
-    }
 
-    class Authorize 
-    {
-        public static bool GetUserAuthorize( User user) 
-        {
-            bool tmp = true;
-            FileInfo file = new FileInfo("users.xml");
-            if (file.Exists)
-            {
-                XmlDocument xdoc = new XmlDocument();
-                xdoc.Load("users.xml");                
-                foreach (XmlElement element in xdoc.GetElementsByTagName("user"))
-                {
-                    if (element.GetElementsByTagName("login")[0].InnerText == user.Login &&
-                        element.GetElementsByTagName("password")[0].InnerText == user.Password)
-                        tmp = false;
-                   
-                }
-                if (ConsoleCommand.AuthTryCount == 3)
-                {
-                    tmp = false;
-                    ConsoleCommand.AuthTryCount = 0;
-                    ConsoleCommand.UserStart();
-                }
-                return tmp;
 
-            }
-            else
-            {
-                return tmp;
-            }
-            
         }
-        public static bool ExistUser(string login)
-        {
-            FileInfo file = new FileInfo("users.xml");
-            if (file.Exists)
-            {
-                XmlDocument xdoc = new XmlDocument();
-                xdoc.Load("users.xml");
-                bool findUser = false;
-                foreach (XmlElement element in xdoc.GetElementsByTagName("user"))
-                {
-                    if (element.GetElementsByTagName("login")[0].InnerText == login)
-                        findUser = true;
-                }
-                return findUser;
-            }
-            else 
-            {
-                return false;
-            }
-            
-        }
-        public static void SaveUserData(string log, string pas, DateTime date)
-        {
-            List<User> users = new List<User>();
-            XElement root;
-            XmlDocument xdoc;
-            FileInfo file = new FileInfo("users.xml");
-            if (!file.Exists)
-            {
-                FileStream fs = file.Create();
-                fs.Close();
-                users.Add(new User(log, pas, date));                
-            }
-            else
-            {
-                xdoc = new XmlDocument();
-                xdoc.Load("users.xml");
-                foreach (XmlElement element in xdoc.GetElementsByTagName("user"))
-                {
-                    users.Add(new User(
-                        element.GetElementsByTagName("login")[0].InnerText,
-                        element.GetElementsByTagName("password")[0].InnerText,
-                        Convert.ToDateTime(element.GetElementsByTagName("dateofbirth")[0].InnerText)));
-                    users.Add(new User(log, pas, date));
-                }
-                xdoc.RemoveAll();
-            }            
-            root = new XElement("users");
-            foreach (User user in users)
-            {
-                XElement element = new XElement("user");
-                element.Add(
-                    new XElement("login", user.Login),
-                    new XElement("password", user.Password),
-                    new XElement("dateofbirth", user.DateOfBirth));
-               root.Add(element);
-            }
-            root.Save("users.xml");            
-        }
-
-    }
-
-    class ConsoleCommand 
-    {
-        public static int AuthTryCount = 0;
-        public static void MustAuth() 
-        {
-            Console.WriteLine("Авторизуйтесь! Заполните поля логина и пароля.");
-        }
-        public static User Auth()
-        {
-            Console.WriteLine("Введите логин:");
-            string login = Console.ReadLine();
-            Console.WriteLine("Введите логин:");
-            string password = Console.ReadLine();
-            User user = new User(login, password,DateTime.Now);
-            return user;
-        }
-        public static void SucessAuth()
-        {
-            Console.WriteLine("Вы успешно авторизовались.");
-        }
-        public static void FailedAuth()
-        {
-            Console.WriteLine("Вы не смогли авторизоваться.");
-            AuthTryCount++;
-        }
-        public static string UserStart() 
-        {
-            Console.WriteLine("Для авторизации наберите auth, для регистрации наберите register");
-            string answer = "";
-            switch (Console.ReadLine())
-            {
-                case "auth": answer = "auth"; break;
-                case "register": answer = "register"; break;
-                default: Console.WriteLine("Введенная вами команда не распознана."); UserStart(); break;
-            }
-            return answer;
-        }
-    }    
+    }  
 }
---
+++ AuthoriseXML/Quiz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthoriseXML
+{
+    internal class Quiz
+    {
+        double Score = 0;
+        public string Name { get; set; }
+
+        List<string> questions = new List<string>() 
+        {   
+            "Что такое ООП?", 
+            "Сколько бит в байте?", 
+            "Вы гениальный программист?", 
+            "Сдадите ли вы экзамен?" 
+        };
+
+        List<string[]> variantsQuestions = new List<string[]>() 
+        { 
+            new string[4] { "Лучшее что со мной происходило", "Не признаю ООП", "Не знаю", "Объектно-ориентированное программирование" }, 
+            new string[4] { "8", "9", "12", "2" }, 
+            new string[4] { "да", "не знаю", "нет", "мама меня все равно любит" }, 
+            new string[4] { "да", "нет", "возможно", "да нет, возможно" } 
+        };
+
+        List<bool[]> rightAnswers = new List<bool[]>()
+        {
+            new bool[4] { false,false,false,true },
+            new bool[4] { true,false,false,false },
+            new bool[4] { true,false,false,true },
+            new bool[4] { true,false,false,false }
+        };
+
+        List<int> ScoreAnswers = new List<int>()
+        {
+            12,12,12,12
+        };
+
+        public Quiz(string name)
+        {
+            Name = name;
+        }
+        
+    }
+}
